31c64e282a74c03ef6e433e800c31f737f0ceb68
Sergiu Falcusan <sergiu@synapsics.io>
2019-11-14 11:08:29 +0200

    fix urls

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/StaticHtml2Generator.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/StaticHtml2Generator.java
@@ -134,4 +134,126 @@ public class StaticHtml2Generator extends DefaultCodegen implements CodegenConfi
         // just return the original string
         return input;
     }
+
+    @Override
+    public void preprocessSwagger(Swagger swagger) {
+        super.preprocessSwagger(swagger);
+
+        if (swagger.getInfo() != null) {
+            Info info = swagger.getInfo();
+            if (StringUtils.isBlank(jsProjectName) && info.getTitle() != null) {
+                // when jsProjectName is not specified, generate it from info.title
+                jsProjectName = sanitizeName(dashize(info.getTitle()));
+            }
+        }
+
+        // default values
+        if (StringUtils.isBlank(jsProjectName)) {
+            jsProjectName = "swagger-js-client";
+        }
+        if (StringUtils.isBlank(jsModuleName)) {
+            jsModuleName = camelize(underscore(jsProjectName));
+        }
+
+        additionalProperties.put("jsProjectName", jsProjectName);
+        additionalProperties.put("jsModuleName", jsModuleName);
+
+        preparHtmlForGlobalDescription(swagger);
+    }
+
+    @Override
+    public CodegenOperation fromOperation(String path, String httpMethod, Operation operation, Map<String, Model> definitions, Swagger swagger) {
+        CodegenOperation op = super.fromOperation(path, httpMethod, operation, definitions, swagger);
+        if (op.returnType != null) {
+            op.returnType = normalizeType(op.returnType);
+        }
+
+        //path is an unescaped variable in the mustache template api.mustache line 82 '<&path>'
+        op.path = sanitizePath(op.path);
+
+        // Set vendor-extension to be used in template:
+        //     x-codegen-hasMoreRequired
+        //     x-codegen-hasMoreOptional
+        //     x-codegen-hasRequiredParams
+        CodegenParameter lastRequired = null;
+        CodegenParameter lastOptional = null;
+        for (CodegenParameter p : op.allParams) {
+            if (p.required) {
+                lastRequired = p;
+            } else {
+                lastOptional = p;
+            }
+        }
+        for (CodegenParameter p : op.allParams) {
+            if (p == lastRequired) {
+                p.vendorExtensions.put("x-codegen-hasMoreRequired", false);
+            } else if (p == lastOptional) {
+                p.vendorExtensions.put("x-codegen-hasMoreOptional", false);
+            } else {
+                p.vendorExtensions.put("x-codegen-hasMoreRequired", true);
+                p.vendorExtensions.put("x-codegen-hasMoreOptional", true);
+            }
+        }
+        op.vendorExtensions.put("x-codegen-hasRequiredParams", lastRequired != null);
+
+        op.vendorExtensions.put("x-codegen-httpMethodUpperCase", httpMethod.toUpperCase());
+
+        return op;
+    }
+
+    /**
+     * Parse Markdown to HTML for the main "Description" attribute
+     *
+     * @param swagger The base object containing the global description through "Info" class
+     * @return Void
+     */
+    private void preparHtmlForGlobalDescription(Swagger swagger) {
+        String currentDescription = swagger.getInfo().getDescription();
+        if (currentDescription != null && !currentDescription.isEmpty()) {
+            Markdown markInstance = new Markdown();
+            swagger.getInfo().setDescription( markInstance.toHtml(currentDescription) );
+        } else {
+            LOGGER.error("Swagger object description is empty [" + swagger.getInfo().getTitle() + "]");
+        }
+    }
+
+    /**
+     * Format to HTML the enums contained in every operations
+     *
+     * @param parameterList The whole parameters contained in one operation
+     * @return String | Html formated enum
+     */
+    public List<CodegenParameter> postProcessParameterEnum(List<CodegenParameter> parameterList) {
+        String enumFormatted = "";
+        for(CodegenParameter parameter : parameterList) {
+            if (parameter.isEnum) {
+                for (int i = 0; i < parameter._enum.size(); i++) {
+                    String spacer = (i == (parameter._enum.size() - 1)) ? " " : ", ";
+
+                    if (parameter._enum.get(i) != null)
+                        enumFormatted += "`" + parameter._enum.get(i) + "`" + spacer;
+                }
+                Markdown markInstance = new Markdown();
+                if (!enumFormatted.isEmpty())
+                    parameter.vendorExtensions.put("x-eumFormatted", markInstance.toHtml(enumFormatted));
+            }
+        }
+        return parameterList;
+    }
+
+
+    private String sanitizePath(String p) {
+        //prefer replace a ', instead of a fuLL URL encode for readability
+        return p.replaceAll("'", "%27");
+    }
+
+    /**
+     * Normalize type by wrapping primitive types with single quotes.
+     *
+     * @param type Primitive type
+     * @return Normalized type
+     */
+    public String normalizeType(String type) {
+        return type.replaceAll("\\b(Boolean|Integer|Number|String|Date)\\b", "'$1'");
+    }
 }
